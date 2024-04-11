b02e1b248bd5d35a0b9c328174ed675684f5ca8c
tiger <tiger@macbookuli.local>
2024-04-11 21:53:54 +0200

    Prepare v1.1.2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'leotiger/grav-plugin-studioeditor',
+        'pretty_version' => '1.1.1',
+        'version' => '1.1.1.0',
+        'reference' => NULL,
+        'type' => 'grav-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'leotiger/grav-plugin-studioeditor' => array(
+            'pretty_version' => '1.1.1',
+            'version' => '1.1.1.0',
+            'reference' => NULL,
+            'type' => 'grav-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
